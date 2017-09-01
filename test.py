68992837a394e8ea97acbae9ba8c6c4a453ffa3a
nownabe <nownabe@gmail.com>
2017-09-01 15:48:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -11,15 +11,6 @@ corpus = ln.load("data", parse=True, cleaner=cleaner)
 
 for cat in corpus.categories:
     for i in range(10):
-        # print("\n<Title>")
-        # print(a.title)
-        # print("\n<Category>")
-        # print(a.category)
-        # print("\n<Timestamp>")
-        # print(a.timestamp)
-        # print("\n<URL>")
-        # print(a.url)
-
         print("\n<<<No Preprocess>>>")
         print("Title: " + nothing.articles[cat][i].title + "\n")
         print("\n".join(nothing.articles[cat][i].content))
