6c5ed006af509f22e8c9b7a8a30c44808c63df8c
nownabe <nownabe@gmail.com>
2017-09-04 11:53:48 +0900

    tokenize

--- test.py
+++ test.py
@@ -6,17 +6,42 @@ from preprocessor import cleaner
 from preprocessor import normalizer
 from preprocessor import segmenter
 
+from JapaneseTokenizer import MecabWrapper
+
 cleaner = cleaner.Basic()
 normalizer = normalizer.Basic()
 segmenter = segmenter.Basic()
 
-nothing = ln.load("data", parse=True)
+# nothing = ln.load("data", parse=True)
 corpus = ln.load("data",
                  parse=True,
                  cleaner=cleaner,
                  normalizer=normalizer,
-                 segmenter=segmenter)
+                 segmenter=segmenter,
+                 limit=100,
+                 )
 
+mecab = MecabWrapper(dictType="neologd")
+
+for i in range(10):
+    for cat in corpus.categories:
+        article = corpus.articles[cat][i]
+        print("Title: " + article.title + "\n")
+
+        for sentence in article.content:
+            tokenized_sentence = mecab.tokenize(sentence=sentence)
+
+            print("[", end="")
+            for token in tokenized_sentence.tokenized_objects:
+                print(f"{token.word_surface}:{token.word_stem}:{token.tuple_pos}", end=", ")
+
+            print("]")
+
+        print("\n================================")
+        input("Continue to enter any key.")
+
+
+"""
 for cat in corpus.categories:
     for i in range(10):
         print("\n<<<No Preprocess>>>")
@@ -30,3 +55,4 @@ for cat in corpus.categories:
         print("\n================================")
 
         input("Continue to enter any key.")
+"""
